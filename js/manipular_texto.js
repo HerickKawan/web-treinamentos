cbcc302e3ae861db92f98e40e7b3c38e031e0c35
HerickKawan <herickkawansilva@gmail.com>
2023-10-18 17:28:38 -0300

    cacular_media js

---
+++ js/manipular_texto.js
@@ -0,0 +1,15 @@
+var message, email
+
+// message = 'Estou estudando javascript'
+// console.log(message.substring(0,9))
+// console.log('Tamanho da mensagem: ',message.length)
+
+email = 'RenatoCosta2023@gmail.com'
+
+console.log(email.substring(0, email.indexOf('@')).toLowerCase())
+// console.log(email.substring(email.indexOf('@')))
+// console.log('BUSCAR CARACTER PELO INDICE', email.charAt(15))
+
+var nomePessoa = 'ANA PAULA'
+
+console.log('Nome Pessoa', nomePessoa.toLowerCase())
