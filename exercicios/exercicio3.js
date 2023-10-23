9085226e4be97a4773a652d0b2b0acd36f4447f0
HerickKawan <herickkawansilva@gmail.com>
2023-10-23 15:05:39 -0300

    exercicios

--- exercicios/exercicio3.js
+++ exercicios/exercicio3.js
@@ -4,7 +4,7 @@ var numA, numB
 numA = 32
 numB = 42
 
-var numA = input.question('informe o primeiro numero?')
-var numB = input.question('informe o segundo numero?')
+numA = input.question('informe o primeiro numero?')
+numB = input.question('informe o segundo numero?')
 
 console.log(`olá, ${numB} ${numA}`)
---
+++ exercicios/exercicio4.js
@@ -0,0 +1,13 @@
+
+var notaProva1 = 8;   
+var notaProva2 = 7;   
+var notaProva3 = 9;   
+var pesoProva1 = 2;  
+var pesoProva2 = 3;  
+var pesoProva3 = 5;   
+
+
+var mediaFinal = (notaProva1 * pesoProva1 + notaProva2 * pesoProva2 + notaProva3 * pesoProva3) / (pesoProva1 + pesoProva2 + pesoProva3);
+
+
+console.log("A média final do aluno é: " + mediaFinal);
